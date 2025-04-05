741006124633fddccc594602ef0dc8519bbc00db
Joseph Tilden <joe.w.tilden@gmail.com>
2025-04-05 19:19:08 -0400

    setup

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<Product>();
+        String tradeData = "./TradeData";
+
+        // Read da file
+        try {
+            Scanner scan = new Scanner(new FileInputStream(tradeData));
+            
+
+        } catch (FileNotFoundException e) {
+            // DO STUFF
+        }
+
+
+
+        /*
+        I-I-I be poppin bottles....
+         */
+
+
 
     }
 }
