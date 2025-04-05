27dcca9e7b82697824b559b4eab7af7ea8c3089b
Joseph Tilden <joe.w.tilden@gmail.com>
2025-04-05 19:03:44 -0400

    aaaaa

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<Product>();
-        
+
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private String productName = "";
+    private String country = "";
+    private String category = "";
+    private double initialPrice = 0;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+}
