9a1eaa77a2496ee9c2afd91c6bdeb7196508bd6f
Joseph Tilden <joe.w.tilden@gmail.com>
2025-04-05 18:59:19 -0400

    paroducts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<Product> products = new ArrayList<Product>();
+        
     }
 }
